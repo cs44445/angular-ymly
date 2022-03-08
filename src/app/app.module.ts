8d7a38871ab4c429a40965a1206d0357f99df35f
cs44445 <2475546459@qq.com>
2022-03-08 16:13:31 +0800

    完成header头部布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core.module';
 
@@ -10,9 +8,8 @@ import { CoreModule } from './core.module';
     AppComponent
   ],
   imports: [
-    // BrowserModule,
-    AppRoutingModule,
-    CoreModule
+    CoreModule,
+    //引入了一个模块，只能使用模块中的服务或者exports导出的东西，所以要使用headerComponent，需要在CoreModule中导出
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,18 +2,26 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './layouts/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+    AppRoutingModule,
+    HeaderComponent
   ]
 })
 export class CoreModule {
+  // SkipSelf跳过自己不让引用自身模块；Optional允许首次父级引用为空
   constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
     if (parentModule) {
       throw new Error('CoreModule只能被AppModule引入');
