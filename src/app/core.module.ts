80bb5fcf8b10c241b97195a6b18df4ad12f5d60d
cs44445 <2475546459@qq.com>
2022-03-08 14:32:08 +0800

    引入图标和全局样式

---
+++ src/app/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule
+  ]
+})
+export class CoreModule {
+  constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule只能被AppModule引入');
+    }
+  }
+}
